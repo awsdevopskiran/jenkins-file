243985ccf987d13e6f87744527f0e2be68aa0b97
awsdevopskiran <111186825+awsdevopskiran@users.noreply.github.com>
2023-06-29 16:19:48 +0530

    Update SQS.tf

--- kyc-ocr-terraform/SQS.tf
+++ kyc-ocr-terraform/SQS.tf
@@ -7,23 +7,10 @@ locals {
   }
 }
 
-resource "aws_sqs_queue" "dlq" {
-  name = "${var.stage}-${var.name_prefix}-dead-letter-queue"
-  tags = var.generic_tags
-}
-
-resource "aws_sqs_queue" "logger" {
-  name = "${var.stage}-${var.name_prefix}-logger-queue"
-  message_retention_seconds   = 14*24*60*60
-  receive_wait_time_seconds   = 0
-  visibility_timeout_seconds  = 30
-  tags = var.generic_tags
-}
 
 resource "aws_sqs_queue" "staging_queues" {
   for_each                    = local.queues
-  name                        = "${var.stage}-${var.name_prefix}-${each.key}-staging-queue.fifo"
-  fifo_queue                  = true
+  name                        = "kyc-ocr-staging-queue.fifo"
   message_retention_seconds   = 60
   receive_wait_time_seconds   = 0
   content_based_deduplication = true
@@ -33,14 +20,10 @@ resource "aws_sqs_queue" "staging_queues" {
 
 resource "aws_sqs_queue" "execution_queues" {
   for_each                   = local.queues
-  name                       = "${var.stage}-${var.name_prefix}-${each.key}-execution-queue"
+  name                       = "kyc-ocr-execution-queue"
   message_retention_seconds  = 60
   receive_wait_time_seconds  = 0
   visibility_timeout_seconds = 300
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.dlq.arn
-    maxReceiveCount     = 1
-  })
 
   tags = var.generic_tags
 }
